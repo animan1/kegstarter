dd614799cca3de7b736ba8440c1399e1641d53c7
Daniel Boeve <djboeve@gmail.com>
2014-03-08 14:34:38 -0500

    Implements login/logout URLs

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+from google.appengine.api import users
 
-app = webapp2.WSGIApplication([('/', MainHandler)],
-                              debug=True)
+class LoginPage(webapp2.RequestHandler):
+    def get(self):
+        self.redirect(users.create_login_url(self.request.uri))
+
+class LogoutPage(webapp2.RequestHandler):
+    def get(self):
+        self.redirect(users.create_login_url('/main'))
+
+urls = [
+    ('/login', LoginPage),
+    ('/logout', LogoutPage),
+]
+app = webapp2.WSGIApplication(urls, debug=True)
